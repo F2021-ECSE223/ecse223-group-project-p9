cf3f79c7906916e9a18df5f14cfd59622e6dfe34
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2021-10-18 14:05:27 -0400

    1st Given: the_following_climb_safe_system_exists_p9

--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P9StepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P9StepDefinitions.java
@@ -5,16 +5,15 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class P9StepDefinitions {
+	private ClimbSafe climbSafe;
+	private String error;
+	private int errorCntr;
+	
   @Given("the following ClimbSafe system exists: \\(p9)")
   public void the_following_climb_safe_system_exists_p9(io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+	  climbSafe = ClimbSafeApplication.getClimbSafe();
+	  error = "";
+	  errorCntr = 0;
   }
 
   @Given("the following equipment exists in the system: \\(p9)")

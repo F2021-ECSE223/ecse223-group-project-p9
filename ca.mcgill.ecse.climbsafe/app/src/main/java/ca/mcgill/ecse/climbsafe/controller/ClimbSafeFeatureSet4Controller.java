f66bf7e6f552cf581fe3325de51bf89be1eb751e
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-10-19 13:46:51 -0400

    Update ClimbSafeFeatureSet4Controller.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -4,6 +4,11 @@ public class ClimbSafeFeatureSet4Controller {
 
   public static void addEquipment(String name, int weight, int pricePerWeek)
       throws InvalidInputException {}
+      throws InvalidInputException {
+	  	
+	    if (equipment.contains(aEquipment)) { return false; }
+	    ClimbSafe existingClimbSafe = aEquipment.getClimbSafe();
+  }
 
   public static void updateEquipment(String oldName, String newName, int newWeight,
       int newPricePerWeek) throws InvalidInputException {}

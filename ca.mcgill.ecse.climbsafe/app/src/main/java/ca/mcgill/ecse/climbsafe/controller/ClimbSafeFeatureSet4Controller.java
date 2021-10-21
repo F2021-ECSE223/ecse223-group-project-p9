81913485c769dbfa08eda2366aacd404d34eefd0
karaebest <kara.best@mail.mcgill.ca>
2021-10-20 20:14:27 -0400

    debug

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -5,8 +5,8 @@ public class ClimbSafeFeatureSet4Controller {
   public static void addEquipment(String name, int weight, int pricePerWeek)
       throws InvalidInputException {
 	  	
-	    if (equipment.contains(aEquipment)) { return false; }
-	    ClimbSafe existingClimbSafe = aEquipment.getClimbSafe();
+	    //if (equipment.contains(aEquipment)) { return false; }
+	    //ClimbSafe existingClimbSafe = aEquipment.getClimbSafe();
   }
 
   public static void updateEquipment(String oldName, String newName, int newWeight,

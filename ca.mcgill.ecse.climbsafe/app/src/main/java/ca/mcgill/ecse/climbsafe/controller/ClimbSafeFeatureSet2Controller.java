0fb3cb2cc2889399eec062310985cd1fa3a70a25
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2021-10-24 15:56:55 -0400

    Set2Controller Register Member DONE

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
@@ -13,16 +13,78 @@ public class ClimbSafeFeatureSet2Controller {
       List<String> itemNames, List<Integer> itemQuantities) throws InvalidInputException {
 	  ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 	  try {
-		  if(email.equals("user3@mail.ca")||email.equals("user4@mail.ca")) {
-			  Member a = climbSafe.addMember(email, password, name, emergencyContact, nrWeeks, guideRequired, hotelRequired);
-			  //climbSafe.addMember(a);//maybe take out
-			  for (int i=0; i<itemNames.size(); i++) {
-				 climbSafe.addBookedItem(itemQuantities.get(i), a, BookableItem.getWithName(itemNames.get(i)));
-				// climbSafe.addBooked(quantity, member, bookableitem)
-			  //a.addBookedItem(new BookedItem(itemQuantities.get(i), climbSafe, a, BookableItem.getWithName(itemNames.get(i))));
-			  
-			  
+		  boolean validEmail = true;
+		  
+		  if(email.contains(" ")) {
+			  validEmail = false;
+		  }
+		  if(!(email.indexOf("@") > 0)) {
+			  validEmail = false;
+		  }
+		  if(!(email.indexOf("@") == email.lastIndexOf("@"))) {
+			  validEmail = false;
+		  }
+		  if(!(email.indexOf("@") < email.lastIndexOf(".") - 1)) {
+			  validEmail = false;
+		  }
+		  if(!(email.lastIndexOf(".") < email.length() - 1)) {
+			  validEmail = false;
+		  }
+		  
+//		  Since admin is already pre-registered, they will not be needing to "register" again, therefore, a member can never be "admin@nmc.nt"
+		  boolean notAdmin = true;
+		  if(email.equals("admin@nmc.nt")) {
+			  notAdmin = false;
+		  }
+		  
+		  boolean validPassword = true;
+		  if(password.equals("")) {
+			  validPassword = false;
+		  }
+		  if(password.equals(null)) {
+			  validPassword = false;
+		  }
+		  
+		  boolean validName = true;
+		  if(name.equals("")) {
+			  validName = false;
+		  }
+		  if(name.equals(null)) {
+			  validName = false;
+		  }
+		  
+		  boolean validEmergencyContact = true;
+		  if(emergencyContact.equals("")) {
+			  validName = false;
+		  }
+		  if(emergencyContact.equals(null)) {
+			  validName = false;
+		  }
+		  
+		  
+		  boolean validNrWeeks = true;
+		  if(nrWeeks<=0) {
+			  validNrWeeks = false;
+		  }
+		  if(nrWeeks > climbSafe.getNrWeeks()) {
+			  validNrWeeks = false;
+		  }
+		  
+		  if(validEmail && validPassword && validName && validEmergencyContact && validNrWeeks && notAdmin) {
+			  boolean validItems = true;
+			  for(int i=0; i<itemNames.size(); i++) {
+				  if(BookableItem.getWithName(itemNames.get(i)) == null){
+					  validItems = false;
+					  break;
+				  }
 			  }
+			  if(validItems) {
+				  Member member = climbSafe.addMember(email, password, name, emergencyContact, nrWeeks, guideRequired, hotelRequired);
+				  for (int i=0; i<itemNames.size(); i++) {
+					 climbSafe.addBookedItem(itemQuantities.get(i), member, BookableItem.getWithName(itemNames.get(i))); 
+				  }
+			  }
+			  
 		  }
 		  
 	  }catch(RuntimeException e){
@@ -34,6 +96,9 @@ public class ClimbSafeFeatureSet2Controller {
   public static void updateMember(String email, String newPassword, String newName,
       String newEmergencyContact, int newNrWeeks, boolean newGuideRequired,
       boolean newHotelRequired, List<String> newItemNames, List<Integer> newItemQuantities)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+	  
+	  
+  }
 
 }

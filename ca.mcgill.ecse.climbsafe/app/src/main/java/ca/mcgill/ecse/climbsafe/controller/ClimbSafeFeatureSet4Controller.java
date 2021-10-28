574c0fcd7be92bb1ebab92b53fdee0ea6d371844
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-10-28 11:45:12 -0400

    Equipment Controller methods update/add

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -27,6 +27,7 @@ public class ClimbSafeFeatureSet4Controller {
       throws InvalidInputException {
 	  String error = "";
 	  ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+	  List<Equipment> myEquipments = climbSafe.getEquipment();
 	  
 	  //checks the constraints
 	  if (weight <= 0) {
@@ -35,21 +36,31 @@ public class ClimbSafeFeatureSet4Controller {
 	  if (pricePerWeek < 0) {
 		  error="The price per week must be greater than or equal to 0 ";
 	  }
-	  
-	  
-	 if (name.equals("") || name.equals(null)) {
+	  if (name.equals("") || name == null) {
 		 error="The name must not be empty";
-	 }
+	  }
 	  
+	  if(validEquipment(myEquipments, name) != -1) {
+		  error = "The piece of equipment already exists";
+	  }
+	  
+	  //Checks if an EquipmentBundle with the same name already exists
+	  List<EquipmentBundle> bundles = climbSafe.getBundles();
+	  for (EquipmentBundle b : bundles) {
+		  if(b.getName().equals(name)) {
+			  error="The equipment bundle already exists";
+		  }
+	  }
+	  
+	  if(error.length() != 0) {
+		  throw new InvalidInputException(error.trim());
+	  }
+	 
 	  //try adding the equipment to the ClimbSafe application and catch errors if any
 	  try {
 		  climbSafe.addEquipment(name, weight, pricePerWeek);
 	  }catch (RuntimeException e) {
-		  error = e.getMessage();
-	      if (error.startsWith("Unable to create equipment due to climbSafe.")) {
-	        error = "The piece of equipment already exists";
-	      }
-	      throw new InvalidInputException(error);
+	      throw new InvalidInputException(e.getMessage());
 	  }
 }
 
@@ -66,40 +77,54 @@ public class ClimbSafeFeatureSet4Controller {
   public static void updateEquipment(String oldName, String newName, int newWeight,
       int newPricePerWeek) throws InvalidInputException {
 	  
-	  // Get the desired equipment in the ClimbSafe application
-	  Equipment foundEquipment = findEquipment(oldName);
-		if (foundEquipment == null) {
-			throw new InvalidInputException("The piece of equipment does not exist");
-		}
-		
 	  String error = "";
 	  ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+	  List<Equipment> myEquipments = climbSafe.getEquipment();
+	  
+	  // Check if the equipment we want to update exists in the ClimbSafe application
+	  if ((validEquipment(myEquipments, oldName))==-1) {
+		error="The piece of equipment does not exist";
+	  }
 	  
 	  //Check the constraints on the updated weight and price 
 	  if (newWeight <= 0) {
-		  error="The weight must be greater than 0. ";
+		  error="The weight must be greater than 0";
 	  }
 	  if (newPricePerWeek < 0) {
-		  error="The price per week cannot be lower than 0. ";
+		  error="The price per week must be greater than or equal to 0";
 	  }
-	  if (newName.equals("") || newName.equals(null)) {
+	  
+	  if (newName.equals("") || newName == null) {
 			 error="The name must not be empty";
 		 }
-	 
+	  
 	  //Checks if an equipment with the same name already exists
 	  List<Equipment> equipments = climbSafe.getEquipment();
 	  for (Equipment e : equipments) {
-			if ((e.getName()).equals(foundEquipment.getName())) {
-				throw new InvalidInputException("The piece of equipment already exists");
+			if ((e.getName()).equals(newName)) {
+				error="The piece of equipment already exists";
 			}
 		}
+	  //Checks if an EquipmentBundle with the same name already exists
+	  List<EquipmentBundle> bundles = climbSafe.getBundles();
+	  for (EquipmentBundle b : bundles) {
+		  if(b.getName().equals(newName)) {
+			  error="An equipment bundle with the same name already exists";
+		  }
+	  }
+	  if(error.length() != 0) {
+		  throw new InvalidInputException(error.trim());
+	  }
 	  
-	  
-	 //update the old equipments' data to the new desired data.
-	  
-	 foundEquipment.setName(newName);
-	 foundEquipment.setPricePerWeek(newPricePerWeek);
-	 foundEquipment.setWeight(newWeight);
+	 //update the old equipment's data to the new desired data.
+	 try {
+		 Equipment myEquipment = climbSafe.getEquipment(validEquipment(myEquipments, oldName));
+		 myEquipment.setName(newName);
+		 myEquipment.setPricePerWeek(newPricePerWeek);
+		 myEquipment.setWeight(newWeight);
+	 }catch(RuntimeException e){
+		  throw new InvalidInputException(e.getMessage());
+	  }
   }
   
   
@@ -112,15 +137,12 @@ public class ClimbSafeFeatureSet4Controller {
  * @return null if no equipment with this name are in the ClimbSafe Application, 
  * otherwise it returns the equipment.
  */
-private static Equipment findEquipment(String oldName) {
-	 ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
-	 List<Equipment> myEquipments = climbSafe.getEquipment();
-	 for(Equipment e : myEquipments) {
-		 String name = e.getName();
-		 if (name.equals(oldName)) {
-			 return e;
+private static int validEquipment (List <Equipment> e, String oldName) {
+	 for(int i=0; i<e.size();i++) {
+		 if ((e.get(i).getName()).equals(oldName)) {
+			 return i;
 		 }
 	 }
-	return null;
+	return -1;
 }
 }

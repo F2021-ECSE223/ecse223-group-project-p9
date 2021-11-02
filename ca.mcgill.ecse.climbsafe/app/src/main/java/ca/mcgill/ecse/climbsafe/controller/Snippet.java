26e555f35b60589fd204126f43f0cbd89cf164ca
karaebest <kara.best@mail.mcgill.ca>
2021-11-02 18:50:41 -0400

    new file deleted

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/Snippet.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse.climbsafe.controller;
+
+public class Snippet {
+	public static void main(String[] args) {
+		
+		AgtBplusOne <='0';
+		AgteBplusOne<='0';
+		AltBplusOne	<='0';
+		AlteBplusOne<='0';
+		AeqBplusOne	<='0';
+		overflow		<='0';
+	}
+}
+

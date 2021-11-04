23384fe5a71cc2223d048773e63cfd59b5a32722
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-11-04 15:29:58 -0400

    CancelTrip

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,17 +1,22 @@
 package ca.mcgill.ecse.climbsafe.controller;
 import ca.mcgill.ecse.climbsafe.model.*;
+import ca.mcgill.ecse.climbsafe.model.Assignment.TripStatus;
 
-
+import java.util.List;
 
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
-import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
 
 public class AssignmentController {
 	//initiate assignments
 	//start trips
-	public static void cancelTrip() {
-		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+	public static void cancelTrip(Assignment assignment) {
+		List<Assignment> myAssignemnts = climbSafe.getAssignments();
 		
+		for (Assignment a : myAssignemnts) {
+			if (a.getMember().equals(assignment.getMember())) {
+				a.setTripStatus(Cancelled);
+			}
+		}
 	}
 	//finish trip
 	public static void finishTrip(Assignment assignment) {

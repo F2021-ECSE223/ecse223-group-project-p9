dc1a660fe173bbdbc7e6a69114a441170852a6a4
karaebest <kara.best@mail.mcgill.ca>
2021-11-04 15:09:29 -0400

    jfjkaekfe

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 public class AssignmentController {
-
+	//initiate assignment
+	//start trips
+	//cancel trip
+	//finish trip
+	//pay for a trip
+	
 }

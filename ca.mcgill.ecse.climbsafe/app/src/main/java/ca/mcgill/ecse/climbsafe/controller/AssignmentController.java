48a71476647282b1592f7f96cffb50d6a013f7f5
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-11-04 16:00:14 -0400

    CancelTrip

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,9 +1,7 @@
 package ca.mcgill.ecse.climbsafe.controller;
 import ca.mcgill.ecse.climbsafe.model.*;
 import ca.mcgill.ecse.climbsafe.model.Assignment.TripStatus;
-
 import java.util.List;
-
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 
 public class AssignmentController { 
@@ -14,18 +12,56 @@ public class AssignmentController {
 	}
 	//start trips
 	public static void cancelTrip(Assignment assignment) {
-		List<Assignment> myAssignemnts = climbSafe.getAssignments();
+		String error="";
+		String email = assignment.getMember().getEmail();
 		
-		for (Assignment a : myAssignemnts) {
-			if (a.getMember().equals(assignment.getMember())) {
-				a.setTripStatus(Cancelled);
-			}
+		if (!validEmail(email)) {
+			error="Member with email address "+ email +" does not exist";
 		}
-	}
+		
+		if(error.length() != 0) {
+			  throw new InvalidInputException(error.trim());
+		  }
+		
+		try {
+			List<Assignment> myAssignments = climbSafe.getAssignments();
+			for (Assignment a : myAssignments) {
+				if (a.getMember().getEmail().equals(assignment.getMember().getEmail())) {
+					a.setTripStatus(TripStatus.Cancelled);
+				}
+			}
+		 }catch(RuntimeException e){
+			  throw new InvalidInputException(e.getMessage());
+		  }
+		}
+
 	//finish trip
 	public static void finishTrip(Assignment assignment) {
 		
 	}
 	//pay for a trip
 	
+	/**
+	 * checking if the email is valid or not
+	 * 
+	 * @author Joey Koay
+	 * @param email
+	 * @return whether the email is valid
+	 */
+  private static boolean validEmail(String email) {
+	  boolean validEmail = true;
+	  if(!(email.indexOf("@") > 0)) {
+		  validEmail = false;
+	  }
+	  if(!(email.indexOf("@") == email.lastIndexOf("@"))) {
+		  validEmail = false;
+	  }
+	  if(!(email.indexOf("@") < email.lastIndexOf(".") - 1)) {
+		  validEmail = false;
+	  }
+	  if(!(email.lastIndexOf(".") < email.length() - 1)) {
+		  validEmail = false;
+	  }
+	  return validEmail;
+  }
 }

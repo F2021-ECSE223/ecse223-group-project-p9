fec2ff0ab5bf430f485feec0392285b947836fc2
karaebest <kara.best@mail.mcgill.ca>
2021-11-04 15:14:05 -0400

    import statement

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,10 +1,17 @@
 package ca.mcgill.ecse.climbsafe.controller;
+import ca.mcgill.ecse.climbsafe.model.*;
+
+
 
 public class AssignmentController {
-	//initiate assignment
+	//initiate assignments
 	//start trips
 	//cancel trip
+	
 	//finish trip
+	public static void finishTrip(Assignment assignment) {
+		
+	}
 	//pay for a trip
 	
 }

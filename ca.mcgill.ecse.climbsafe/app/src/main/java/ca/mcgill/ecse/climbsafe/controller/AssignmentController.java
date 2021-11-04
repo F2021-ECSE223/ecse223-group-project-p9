6a147f1ceed4f982ba35e33ebdb0387138ca03fa
ysj3090 <82122563+ysj3090@users.noreply.github.com>
2021-11-04 15:30:32 -0400

    Initiate Assignment

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -6,8 +6,12 @@ import ca.mcgill.ecse.climbsafe.model.*;
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
 
-public class AssignmentController {
-	//initiate assignments
+public class AssignmentController { 
+	public static void initiateAssignment (int startWeek, int endWeek, int nrWeeks, int totalCostForGuide, int totalCostForEquipment, boolean guideRequired) 
+			throws InvalidInputException {
+		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+		
+	}
 	//start trips
 	public static void cancelTrip() {
 		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();

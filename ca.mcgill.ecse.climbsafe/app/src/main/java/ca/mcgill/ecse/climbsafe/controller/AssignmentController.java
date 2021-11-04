df4752ae181843dd5d894033bcbf6eaa92485667
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-11-04 15:14:38 -0400

    CancelTrip

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,9 +1,15 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+
 public class AssignmentController {
 	//initiate assignment
 	//start trips
-	//cancel trip
+	public static void cancelTrip() {
+		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+		
+	}
 	//finish trip
 	//pay for a trip
 	

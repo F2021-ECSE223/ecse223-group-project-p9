814183216b9e0bc6c90e68918eff33f5ae63b3e7
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2021-11-11 15:08:17 -0500

    persistence packages init

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/application/ClimbSafeApplication.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/application/ClimbSafeApplication.java
@@ -3,8 +3,8 @@
  */
 package ca.mcgill.ecse.climbsafe.application;
 
-import java.sql.Date;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.persistence.ClimbSafePersistence;
 
 public class ClimbSafeApplication {
   private static ClimbSafe climbSafe;
@@ -20,9 +20,7 @@ public class ClimbSafeApplication {
   public static ClimbSafe getClimbSafe() {
     if (climbSafe == null) {
       // these attributes are default, you should set them later with the setters
-      climbSafe = new ClimbSafe(new Date(0), 0, 0);
-//      btms = BtmsPersistence.load();
-//      make this climbSafe
+      climbSafe = ClimbSafePersistence.load();
     }
     
     return climbSafe;

1d6df8c7b47b48a9b6b81c2f1ed4a20efeb32b1a
karaebest <kara.best@mail.mcgill.ca>
2021-11-27 22:53:25 -0500

    payformemberTrip

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/PayForMemberTripPageController.java
@@ -0,0 +1,32 @@
+import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
+
+import ca.mcgill.ecse.climbsafe.controller.*;
+import ca.mcgill.ecse.climbsafe.model.*;
+
+public class PayForMemberTripPageController {
+  @FXML
+  private TextField memberEmailTextField;
+  @FXML
+  private TextField codeTextField;
+  @FXML
+  private Button payForTripButton;
+
+
+  // Event Listener on Button[#addDriverButton].onAction
+  @FXML
+  public void payClicked(ActionEvent event) {
+    String memberEmail = memberEmailTextField.getText();
+    String code = codeTextField.getText();
+    try {
+    	
+		if(successful(() -> )) {
+			numWeeksTextField.setText("");
+			weeklyPriceOfGuideTextField.setText("");
+			//somehow set dateBox to initial state or something
+		}
+	} catch (RuntimeException e) {
+		ViewUtils.showError(e.getMessage());
+	}
+  }
+
+}

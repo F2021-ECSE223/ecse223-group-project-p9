d539da058081d2c23f0976d83349c33a6f58d2f4
karaebest <kara.best@mail.mcgill.ca>
2021-11-29 15:10:12 -0500

    Kara pages edit

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/PayForMemberTripPageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/PayForMemberTripPageController.java
@@ -4,9 +4,11 @@ import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
 
 
 import ca.mcgill.ecse.climbsafe.controller.*;
+import ca.mcgill.ecse.climbsafe.view.ClimbSafeFxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -16,26 +18,37 @@ public class PayForMemberTripPageController {
   @FXML
   private Button payForTripButton;
   @FXML
-  private TextField memberEmailTextField;
-  @FXML
   private Text completeText;
+  @FXML
+  private ChoiceBox<String> memberChoiceBox;
 
-
+  public void initialize() {
+	  memberChoiceBox.addEventHandler(ClimbSafeFxmlView.REFRESH_EVENT, e -> {
+			memberChoiceBox.setItems(ViewUtils.getMembers());
+			memberChoiceBox.setValue(null);
+		});	
+	  ClimbSafeFxmlView.getInstance().registerRefreshEvent(memberChoiceBox);
+	}
 
   @FXML
   public void payClicked(ActionEvent event) {
     String code = codeTextField.getText();
-    String memberEmail = memberEmailTextField.getText();
+    String memberEmail = memberChoiceBox.getValue();
     try {    	
 		if(successful(() -> AssignmentController.payForTrip(memberEmail, code))) {
 			codeTextField.setText("");
-			memberEmailTextField.setText("");
 			completeText.setText("Payment authorized.");
+			ClimbSafeFxmlView.getInstance().registerRefreshEvent(memberChoiceBox);
+
 		}else {
 			completeText.setText("Could not be completed.");
+			ClimbSafeFxmlView.getInstance().registerRefreshEvent(memberChoiceBox);
+
 		}
 	} catch (RuntimeException e) {
 		ViewUtils.showError(e.getMessage());
+		ClimbSafeFxmlView.getInstance().registerRefreshEvent(memberChoiceBox);
+
 	}
   }
   

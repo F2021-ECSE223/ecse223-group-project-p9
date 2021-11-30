53932e88efb6db647a4e73e3f7fc008db47cc489
1313eunjun <eun-jun.chang@mail.mcgill.ca>
2021-11-30 17:49:09 -0500

    .

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateGuidePageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateGuidePageController.java
@@ -1,5 +1,32 @@
 package ca.mcgill.ecse.climbsafe.view.controllers;
 
+import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
+
+import java.util.List;
+
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet2Controller;
+import ca.mcgill.ecse.climbsafe.view.ClimbSafeFxmlView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+//import ca.mcgill.ecse.climbsafe.view.pages.;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+
 public class UpdateGuidePageController {
+	
+	@FXML private TextField name;
+	@FXML private TextField email;
+	@FXML private PasswordField password;
+	@FXML private TextField emergencyContact;
+	@FXML private CheckBox permissionCheck;
+	
 
 }

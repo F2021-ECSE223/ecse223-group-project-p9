9efde91bd345cf451644a8cc7269330979f07859
ysj3090 <82122563+ysj3090@users.noreply.github.com>
2021-11-30 19:09:21 -0500

    UpdateEquipmentBundle Controller

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateEquipmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateEquipmentController.java
@@ -6,7 +6,11 @@ import java.util.List;
 import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet5Controller;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet6Controller;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
 import ca.mcgill.ecse.climbsafe.view.ClimbSafeFxmlView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,26 +21,83 @@ import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 
 public class UpdateEquipmentController {
+	@FXML private TextField addEquipmentBundleName;
+	@FXML private TextField addEquipmentBundlePrice;
+	@FXML private ChoiceBox<Integer> itemNameChoiceBox;
+
+	@FXML private Button addEquipmentButton;
+	@FXML private Spinner<Integer> itemQuantitySpinner;
+	@FXML private Button addEditItemButton;
+	@FXML private ListView<String> equipmentBundleListView;
+	@FXML private Button equipmentBundleSearchButton;
+	@FXML private Button updateEquipmentBundleButton;
+	@FXML private Button deleteEquipmentBundleButton;
 	
 	@FXML private TextField oldNameModifyEquipmentBundle;
 	@FXML private TextField newNameModifyEquipmentBundle;
-	@FXML private TextField priceModifyEquipementBundle;
+	@FXML private TextField priceModifyEquipmentBundle;
 	@FXML private TextField deleteEquipmentBundleName;
-	@FXML private Button modifyEquipmentBundleButton;
-	@FXML private Button deleteEquipmentBundleButton;
 	
-	public void deleteEquipmentBundleClicked(ActionEvent event) {
-		String name = newNameModifyEquipmentBundle.getText();
+	
+	private List<String> itemNames = new ArrayList<>();
+	private List<Integer> itemQuantities = new ArrayList<>();
+	private EquipmentBundle e = null;
+	
+	/**
+	 * @author SeJong Yoo
+	 * @param event
+	 */
+	
+	public void initialize() {
+		addEquipmentBundleName.setText("");
+		itemNameChoiceBox.addEventHandler(ClimbSafeFxmlView.REFRESH_EVENT, e -> {
+			itemNameChoiceBox.setItems(null);
+			itemNameChoiceBox.setValue(null);
+		});
+		itemQuantitySpinner.addEventHandler(ClimbSafeFxmlView.REFRESH_EVENT, e -> {
+			int initQuantity = 0;
+			int minQuantity = 0;
+			int maxQuantity = 99;
+			SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(initQuantity, minQuantity, maxQuantity);
+			itemQuantitySpinner.setValueFactory(valueFactory);
+		});
+		ClimbSafeFxmlView.getInstance().registerRefreshEvent(itemNameChoiceBox);
+		ClimbSafeFxmlView.getInstance().registerRefreshEvent(itemQuantitySpinner);
+		
+	}
+	
+	@FXML
+	public void modifyEquipmentBundleClicked(ActionEvent event) {
+		String oldName = oldNameModifyEquipmentBundle.getText();
+		String newName = newNameModifyEquipmentBundle.getText();
+		int price = Integer.parseInt(priceModifyEquipmentBundle.getText());
 		
 		try {
-			if(successful(() -> ClimbSafeFeatureSet6Controller.deleteEquipment(name))) {
-				deleteEquipmentBundleName.setText("");
+			if(successful(() -> ClimbSafeFeatureSet5Controller.updateEquipmentBundle(oldName, newName, price, itemNames, itemQuantities))) {
+				oldNameModifyEquipmentBundle.setText("");
+				newNameModifyEquipmentBundle.setText("");
+				priceModifyEquipmentBundle.setText("");
 				ClimbSafeFxmlView.getInstance().refresh();
 			}
-			
 		} catch (RuntimeException e) {
 			ViewUtils.showError(e.getMessage());
 		}
 	}
+	
+	@FXML
+	public void deleteEquipmentBundleClicked(ActionEvent event) {
+		String newName = newNameModifyEquipmentBundle.getText();
+		
+		try {
+			if(successful(() -> ClimbSafeFeatureSet6Controller.deleteEquipmentBundle(name))) {
+				deleteEquipmentBundleName.setText("");
+				ClimbSafeFxmlView.getInstance().refresh();
+			}
+			
+		} catch (RuntimeException e)  {
+			ViewUtils.showError(newName);
+		}
+	}
+			
 
 }

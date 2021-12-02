0598964295544b96ad898a36f9587361689662e8
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2021-12-01 19:11:52 -0500

    SetupNMC and StartFinishCancelTrip

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeController.java
@@ -187,4 +187,12 @@ public class ClimbSafeController {
 	  }
 	  return itemsQuantityList;
   }
+  public static List<Integer> getPriceOfGuidePerWeek(){
+	  int weeklyPrice = climbSafe.getPriceOfGuidePerWeek();
+	  List<Integer> weeklyPricesGuide = new ArrayList<>();
+	  for(int i=0; i<=weeklyPrice; i++) {
+		  weeklyPricesGuide.add(i);
+	  }
+	  return weeklyPricesGuide;
+  }
 }

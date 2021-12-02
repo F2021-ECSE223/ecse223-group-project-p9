cb7bab848ba09ea7e5d99c647078a08ef39e0985
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2021-12-02 17:41:40 -0500

    hotfix

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateMemberPageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/UpdateMemberPageController.java
@@ -223,6 +223,7 @@ public class UpdateMemberPageController {
 					itemNameChoiceBox.setValue(null);
 					itemQuantitySpinner.setValueFactory(null);
 					memberItemsListView.setItems(null);
+					deleteConfirmButton.setSelected(false);
 					ClimbSafeFxmlView.getInstance().refresh();
 					updateMessageLabel.setText("Member deleted successfully");
 				}

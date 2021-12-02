0a2f21cedd0acc01624addc6f03cddafd6c77b8a
karaebest <kara.best@mail.mcgill.ca>
2021-12-02 01:11:09 -0500

    view all done

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/ViewAllAssignmentsControllerPage.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/ViewAllAssignmentsControllerPage.java
@@ -5,11 +5,13 @@ import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
 import java.util.List;
 
 import ca.mcgill.ecse.climbsafe.controller.*;
+import ca.mcgill.ecse.climbsafe.view.ClimbSafeFxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
 
 public class ViewAllAssignmentsControllerPage {
 
@@ -41,10 +43,24 @@ public class ViewAllAssignmentsControllerPage {
   private TableColumn<TOTableAssignment, String> equipCostColumn;
   @FXML
   private TableView<TOTableAssignment> assignmentTable;
+  @FXML
+  private Text assignmentCompletionText;
+  
+  public void initialize() {
+	  //add if statement for when no data in system
+	  List<TOAssignment> assignments = ClimbSafeFeatureSet6Controller.getAssignments();
+	  
+	  if(!(assignments.isEmpty())) {
+			 assignmentCompletionText.setText("Assignments initialized.");
+	  }else {
+			 assignmentCompletionText.setText("Please initialize assignments before viewing.");
+	  }
+	}
 
   @FXML
   public void viewClicked(ActionEvent event) {
     List<TOTableAssignment> assignments = ClimbSafeController.getTOTableAssignments();
+    assignmentTable.getItems().clear();
     for(TOTableAssignment a: assignments) {
     	assignmentTable.getItems().add(a);
     }
@@ -53,12 +69,25 @@ public class ViewAllAssignmentsControllerPage {
   // Event Listener on Button[#addRouteButton].onAction
   @FXML
   public void initiateClicked(ActionEvent event) {
-    // omitted
+	  List<TOAssignment> assignments = ClimbSafeFeatureSet6Controller.getAssignments();
+
+		  //need to refresh members/guides in the case where they are updated before assignment
+		  try {
+			  if(assignments.isEmpty()) {
+					 if(successful(() -> AssignmentController.initiateAssignment())) {
+						 assignmentCompletionText.setText("Assignments initialized.");
+					 }
+			  }else {
+				  String e = "Assignments already initialized, cannot initialize again.";
+				  ViewUtils.showError(e);
+			  }
+				
+
+			} catch (RuntimeException e) {
+				ViewUtils.showError(e.getMessage());
+			}
+	  
   }
 
-  // Event Listener on Button[#addBusButton].onAction
-  @FXML
-  public void addBusClicked(ActionEvent event) {
-    // omitted
-  }
+  
 }

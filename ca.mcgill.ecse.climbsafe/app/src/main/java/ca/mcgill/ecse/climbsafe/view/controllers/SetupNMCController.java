f4600f097698e08372c8924df27c2e6537f4b4b3
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2021-12-02 19:33:21 -0500

    SetupNMC Background

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/SetupNMCController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/controllers/SetupNMCController.java
@@ -5,6 +5,7 @@ import static ca.mcgill.ecse.climbsafe.view.controllers.ViewUtils.successful;
 import java.sql.Date;
 import java.time.*;
 
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeController;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet1Controller;
 import ca.mcgill.ecse.climbsafe.view.ClimbSafeFxmlView;
 import javafx.event.ActionEvent;
@@ -13,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.control.ChoiceBox;
 
@@ -23,9 +25,88 @@ public class SetupNMCController {
 	@FXML private DatePicker dateBox;
 	@FXML private Button next;
 	@FXML private Text returnMessageText;
+	@FXML private ImageView winterBackground;
+	@FXML private ImageView fallBackground;
+	@FXML private ImageView summerBackground;
+	@FXML private ImageView springBackground;
+	
+	String myDate = ClimbSafeController.getNMCDate().toString();
+	String month = myDate.split("-")[1];
+	String day = myDate.split("-")[2];
 	
 	@FXML
 	public void initialize() {
+		if (Integer.parseInt(month) < 3) {
+			winterBackground.setOpacity(1);
+			summerBackground.setOpacity(0);
+			fallBackground.setOpacity(0);
+			springBackground.setOpacity(0);
+			
+		}else if (Integer.parseInt(month) == 3) {
+			if (Integer.parseInt(day) < 20) {
+				winterBackground.setOpacity(1);
+				summerBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+			}else {
+				springBackground.setOpacity(1);
+				winterBackground.setOpacity(0);
+				summerBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+			}
+		}else if (Integer.parseInt(month)< 6 && Integer.parseInt(month) > 3) {
+			springBackground.setOpacity(1);
+			winterBackground.setOpacity(0);
+			summerBackground.setOpacity(0);
+			fallBackground.setOpacity(0);
+		}else if (Integer.parseInt(month) == 6) {
+			if (Integer.parseInt(day) < 20) {
+				springBackground.setOpacity(1);
+				winterBackground.setOpacity(0);
+				summerBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+			}else {
+				summerBackground.setOpacity(1);
+				winterBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+			}
+		}else if (Integer.parseInt(month) < 9 && Integer.parseInt(month) > 6) {
+			summerBackground.setOpacity(1);
+			winterBackground.setOpacity(0);
+			springBackground.setOpacity(0);
+			fallBackground.setOpacity(0);
+		}else if(Integer.parseInt(month) == 9) {
+			if (Integer.parseInt(day) < 22) {
+				summerBackground.setOpacity(1);
+				winterBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+			}else {
+				fallBackground.setOpacity(1);
+				summerBackground.setOpacity(0);
+				winterBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+			}
+		}else if (Integer.parseInt(month) < 12 && Integer.parseInt(month) > 9){
+			fallBackground.setOpacity(1);
+			summerBackground.setOpacity(0);
+			winterBackground.setOpacity(0);
+			springBackground.setOpacity(0);
+		}else if(Integer.parseInt(month) == 12) {
+			if (Integer.parseInt(day) < 21) {
+				fallBackground.setOpacity(1);
+				summerBackground.setOpacity(0);
+				winterBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+			}else {
+				winterBackground.setOpacity(1);
+				summerBackground.setOpacity(0);
+				fallBackground.setOpacity(0);
+				springBackground.setOpacity(0);
+			}
+		}
+		
 		nrWeeksSpinner.addEventHandler(ClimbSafeFxmlView.REFRESH_EVENT, e -> {
 			int minQuantity = 0;
 			int maxQuantity = 52;
@@ -62,6 +143,79 @@ public class SetupNMCController {
 					weeklyPriceSpinner.getValueFactory().setValue(0);
 					ClimbSafeFxmlView.getInstance().refresh();
 					returnMessageText.setText("NMC info setup/updated successfully");
+				}
+				myDate = ClimbSafeController.getNMCDate().toString();
+				month = myDate.split("-")[1];
+				day = myDate.split("-")[2];
+				if (Integer.parseInt(month) < 3) {
+					winterBackground.setOpacity(1);
+					summerBackground.setOpacity(0);
+					fallBackground.setOpacity(0);
+					springBackground.setOpacity(0);
+					
+				}else if (Integer.parseInt(month) == 3) {
+					if (Integer.parseInt(day) < 20) {
+						winterBackground.setOpacity(1);
+						summerBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+					}else {
+						springBackground.setOpacity(1);
+						winterBackground.setOpacity(0);
+						summerBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+					}
+				}else if (Integer.parseInt(month)< 6 && Integer.parseInt(month) > 3) {
+					springBackground.setOpacity(1);
+					winterBackground.setOpacity(0);
+					summerBackground.setOpacity(0);
+					fallBackground.setOpacity(0);
+				}else if (Integer.parseInt(month) == 6) {
+					if (Integer.parseInt(day) < 20) {
+						springBackground.setOpacity(1);
+						winterBackground.setOpacity(0);
+						summerBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+					}else {
+						summerBackground.setOpacity(1);
+						winterBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+					}
+				}else if (Integer.parseInt(month) < 9 && Integer.parseInt(month) > 6) {
+					summerBackground.setOpacity(1);
+					winterBackground.setOpacity(0);
+					springBackground.setOpacity(0);
+					fallBackground.setOpacity(0);
+				}else if(Integer.parseInt(month) == 9) {
+					if (Integer.parseInt(day) < 22) {
+						summerBackground.setOpacity(1);
+						winterBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+					}else {
+						fallBackground.setOpacity(1);
+						summerBackground.setOpacity(0);
+						winterBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+					}
+				}else if (Integer.parseInt(month) < 12 && Integer.parseInt(month) > 9){
+					fallBackground.setOpacity(1);
+					summerBackground.setOpacity(0);
+					winterBackground.setOpacity(0);
+					springBackground.setOpacity(0);
+				}else if(Integer.parseInt(month) == 12) {
+					if (Integer.parseInt(day) < 21) {
+						fallBackground.setOpacity(1);
+						summerBackground.setOpacity(0);
+						winterBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+					}else {
+						winterBackground.setOpacity(1);
+						summerBackground.setOpacity(0);
+						fallBackground.setOpacity(0);
+						springBackground.setOpacity(0);
+					}
 				}
 			} catch (RuntimeException e) {
 				ViewUtils.showError(e.getMessage());
